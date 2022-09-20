f899e7a7ee32bac7d2479c266dd8e51166844e1a
zajic <wont@tell.you>
2022-09-20 12:09:44 -0700

    Initial commit - copy Steve Syfuh's repo

---
+++ CredProvider.NET/CredentialProviderCredential.cs
@@ -0,0 +1,285 @@
+﻿using CredProvider.NET.Interop2;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+using static CredProvider.NET.Constants;
+
+namespace CredProvider.NET
+{
+    public class CredentialProviderCredential : ICredentialProviderCredential2
+    {
+        private readonly CredentialView view;
+        private string sid;
+
+        public CredentialProviderCredential(CredentialView view, string sid)
+        {
+            Logger.Write();
+
+            this.view = view;
+            this.sid = sid;
+        }
+
+        public virtual int Advise(ICredentialProviderCredentialEvents pcpce)
+        {
+            Logger.Write();
+
+            if (pcpce is ICredentialProviderCredentialEvents2 ev2)
+            {
+                Logger.Write("pcpce is ICredentialProviderCredentialEvents2");
+            }
+
+            return HRESULT.S_OK;
+        }
+
+        public virtual int UnAdvise()
+        {
+            Logger.Write();
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int SetSelected(out int pbAutoLogon)
+        {
+            Logger.Write();
+
+            //Set this to 1 if you would like GetSerialization called immediately on selection
+            pbAutoLogon = 0;
+
+            return HRESULT.S_OK;
+        }
+
+        public virtual int SetDeselected()
+        {
+            Logger.Write();
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int GetFieldState(
+            uint dwFieldID,
+            out _CREDENTIAL_PROVIDER_FIELD_STATE pcpfs,
+            out _CREDENTIAL_PROVIDER_FIELD_INTERACTIVE_STATE pcpfis
+        )
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            view.GetFieldState((int)dwFieldID, out pcpfs, out pcpfis);
+
+            return HRESULT.S_OK;
+        }
+
+        public virtual int GetStringValue(uint dwFieldID, out string ppsz)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            ppsz = view.GetValue((int)dwFieldID);
+            
+            return HRESULT.S_OK;
+        }
+
+        private Bitmap tileIcon;
+
+        public virtual int GetBitmapValue(uint dwFieldID, out IntPtr phbmp)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            try
+            {
+                TryLoadUserIcon();
+            }
+            catch (Exception ex) 
+            {
+                Logger.Write("Error: " + ex);
+            }
+
+            phbmp = tileIcon?.GetHbitmap() ?? IntPtr.Zero;
+
+            return HRESULT.S_OK;
+        }
+
+        private void TryLoadUserIcon()
+        {
+            if (tileIcon == null)
+            {
+                var fileName = "CredProvider.NET.tile-icon.bmp";
+                var assembly = Assembly.GetExecutingAssembly();
+                var stream = assembly.GetManifestResourceStream(fileName);
+
+                tileIcon = (Bitmap)Image.FromStream(stream);
+            }
+        }
+
+        public virtual int GetCheckboxValue(uint dwFieldID, out int pbChecked, out string ppszLabel)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            pbChecked = 0;
+            ppszLabel = "";
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int GetSubmitButtonValue(uint dwFieldID, out uint pdwAdjacentTo)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            pdwAdjacentTo = 0;
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int GetComboBoxValueCount(uint dwFieldID, out uint pcItems, out uint pdwSelectedItem)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            pcItems = 0;
+            pdwSelectedItem = 0;
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int GetComboBoxValueAt(uint dwFieldID, uint dwItem, out string ppszItem)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}; dwItem: {dwItem}");
+
+            ppszItem = "";
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int SetStringValue(uint dwFieldID, string psz)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}; psz: {psz}");
+
+            view.SetValue((int) dwFieldID, psz);
+
+            return HRESULT.S_OK;
+        }
+
+        public virtual int SetCheckboxValue(uint dwFieldID, int bChecked)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}; bChecked: {bChecked}");
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int SetComboBoxSelectedValue(uint dwFieldID, uint dwSelectedItem)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}; dwSelectedItem: {dwSelectedItem}");
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int CommandLinkClicked(uint dwFieldID)
+        {
+            Logger.Write($"dwFieldID: {dwFieldID}");
+
+            return HRESULT.E_NOTIMPL;
+        }
+
+        public virtual int GetSerialization(
+            out _CREDENTIAL_PROVIDER_GET_SERIALIZATION_RESPONSE pcpgsr,
+            out _CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION pcpcs,
+            out string ppszOptionalStatusText,
+            out _CREDENTIAL_PROVIDER_STATUS_ICON pcpsiOptionalStatusIcon
+        )
+        {
+            Logger.Write();
+
+            var usage = this.view.Provider.GetUsage();
+
+            pcpgsr = _CREDENTIAL_PROVIDER_GET_SERIALIZATION_RESPONSE.CPGSR_NO_CREDENTIAL_NOT_FINISHED;
+            pcpcs = new _CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION();
+            ppszOptionalStatusText = "";
+            pcpsiOptionalStatusIcon = _CREDENTIAL_PROVIDER_STATUS_ICON.CPSI_NONE;
+
+            //Serialization can be called before the user has entered any values. Only applies to logon usage scenarios
+            if (usage == _CREDENTIAL_PROVIDER_USAGE_SCENARIO.CPUS_LOGON || usage == _CREDENTIAL_PROVIDER_USAGE_SCENARIO.CPUS_UNLOCK_WORKSTATION)
+            {
+                //Determine the authentication package
+                Common.RetrieveNegotiateAuthPackage(out var authPackage);
+
+                //Only credential packing for msv1_0 is supported using this code
+                Logger.Write($"Got authentication package: {authPackage}. Only local authenticsation package 0 (msv1_0) is supported.");
+
+                //Get username and password
+                var username = Common.GetNameFromSid(this.sid);
+                GetStringValue(2, out var password);
+
+                Logger.Write($"Preparing to serialise credential with password...");
+                pcpgsr = _CREDENTIAL_PROVIDER_GET_SERIALIZATION_RESPONSE.CPGSR_RETURN_CREDENTIAL_FINISHED;
+                pcpcs = new _CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION();
+
+                var inCredSize = 0;
+                var inCredBuffer = Marshal.AllocCoTaskMem(0);
+
+                //This should work fine in Windows 10 that only uses the Logon scenario
+                //But it could fail for the workstation unlock scanario on older OS's
+                if (!PInvoke.CredPackAuthenticationBuffer(0, username, password, inCredBuffer, ref inCredSize))
+                {
+                    Marshal.FreeCoTaskMem(inCredBuffer);
+                    inCredBuffer = Marshal.AllocCoTaskMem(inCredSize);
+
+                    if (PInvoke.CredPackAuthenticationBuffer(0, username, password, inCredBuffer, ref inCredSize))
+                    {
+                        ppszOptionalStatusText = string.Empty;
+                        pcpsiOptionalStatusIcon = _CREDENTIAL_PROVIDER_STATUS_ICON.CPSI_SUCCESS;
+
+                        //Better to move the CLSID to a constant (but currently used in the .reg file)
+                        pcpcs.clsidCredentialProvider = Guid.Parse("00006d50-0000-0000-b090-00006b0b0000");
+                        pcpcs.rgbSerialization = inCredBuffer;
+                        pcpcs.cbSerialization = (uint)inCredSize;
+                        pcpcs.ulAuthenticationPackage = authPackage;
+
+                        return HRESULT.S_OK;
+                    }
+
+                    ppszOptionalStatusText = "Failed to pack credentials";
+                    pcpsiOptionalStatusIcon = _CREDENTIAL_PROVIDER_STATUS_ICON.CPSI_ERROR;
+                    return HRESULT.E_FAIL;
+                }
+            }
+            //Implement code to change password here. This is not handled natively.
+            else if (usage == _CREDENTIAL_PROVIDER_USAGE_SCENARIO.CPUS_CHANGE_PASSWORD)
+            {
+                pcpgsr = _CREDENTIAL_PROVIDER_GET_SERIALIZATION_RESPONSE.CPGSR_NO_CREDENTIAL_FINISHED;
+                pcpcs = new _CREDENTIAL_PROVIDER_CREDENTIAL_SERIALIZATION();
+                ppszOptionalStatusText = "Password changed success message.";
+                pcpsiOptionalStatusIcon = _CREDENTIAL_PROVIDER_STATUS_ICON.CPSI_SUCCESS;
+            }
+
+            Logger.Write("Returning S_OK");
+            return HRESULT.S_OK;
+        }
+
+        public virtual int ReportResult(
+            int ntsStatus,
+            int ntsSubstatus,
+            out string ppszOptionalStatusText,
+            out _CREDENTIAL_PROVIDER_STATUS_ICON pcpsiOptionalStatusIcon
+        )
+        {
+            Logger.Write($"ntsStatus: {ntsStatus}; ntsSubstatus: {ntsSubstatus}");
+
+            ppszOptionalStatusText = "";
+            pcpsiOptionalStatusIcon = _CREDENTIAL_PROVIDER_STATUS_ICON.CPSI_NONE;
+
+            return HRESULT.S_OK;
+        }
+
+        public virtual int GetUserSid(out string sid)
+        {
+            Logger.Write();
+
+            sid = this.sid;
+
+            Console.WriteLine($"Returning sid: {sid}");
+            return HRESULT.S_OK;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# This isn't maintained anymore
+Feel free to fork the code but any contributions or questions are not guaranteed to be responded to in a timely manner.
+
+# CredProvider.NET
+A Windows Credential Provider written in C#
+
+## Getting Started
+See this https://syfuhs.net/2017/10/15/creating-custom-windows-credential-providers-in-net/
